c0b5ab7d36a97db331d924ab490a84dcd9422fea
Andrey <fxklim@gmail.com>
2015-03-08 14:05:22 +0500

    refactoring

--- index.js
+++ index.js
@@ -2,33 +2,28 @@
  * Created by KlimMalgin on 04.03.2015.
  */
 
-/**
- * Здесь будет описание математических функторов типа
- * add,
- * sub,
- * div
- * и т.д.
- **/
+var curry = require('core.lambda').curry;
 
-var curry = require('core.lambda').curry,
-    Option = require('fantasy-options').Option,
-    // --
-    fcommon = require('./fcommon'),
-    operationWrap = fcommon.operationWrap,
-    conditionResult = fcommon.conditionResult;
+var addOp = curry(2, function (meV, valueV) { return meV + valueV; });
+var subOp = curry(2, function (meV, valueV) { return meV - valueV; });
 
-var addOp = curry(2, function (meV, valueV) { return Option.of(meV + valueV); });
-var subOp = curry(2, function (meV, valueV) { return Option.of(meV - valueV); });
-
-var divOp = curry(2, function (meV, valueV) { return conditionResult(valueV != 0, meV / valueV); });
-var mulOp = curry(2, function (meV, valueV) { return Option.of(meV * valueV); });
+var divOp = curry(2, function (meV, valueV) { return valueV != 0 ? meV / valueV : null; });
+var mulOp = curry(2, function (meV, valueV) { return meV * valueV; });
 
 module.exports = {
 
-    add: operationWrap(addOp),
-    sub: operationWrap(subOp),
+    add: function (valueO) {
+        return this.operation(addOp)(valueO));
+    },
+    sub: function (valueO) {
+        return this.operation(subOp)(valueO);
+    },
 
-    div: operationWrap(divOp),
-    mul: operationWrap(mulOp)
+    div: function (valueO) {
+        return this.operationWrap(divOp)(valueO);
+    },
+    mul: function (valueO) {
+        return this.operationWrap(mulOp)(valueO);
+    }
 
 };
